f0d724cd306c22f0c2ff2f957ef6bf5ad469d930
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-03-22 00:38:44 +0100

    recursion functions

--- python_structure/lists_tuples_dictionaries/list_defs.py
+++ python_structure/lists_tuples_dictionaries/list_defs.py
@@ -1,3 +1,29 @@
+star_wars_movies = ["Episode I - The Phantom Menace",
+                    "Episode II – Attack of the Clones",
+                    "Episode III – Revenge of the Sith",
+                    "Episode IV – A New Hope",
+                    "Episode V – The Empire Strikes Back",
+                    "Episode VI – Return of the Jedi",
+                    "Episode VII – The Force Awakens",
+                    "Episode VIII – The Last Jedi",
+                    "Episode IX – The Rise of Skywalker",
+                    "Rogue One: A Star Wars Story",
+                    "Solo: A Star Wars Story"]
+
+rating_movies = ["I love Episode I",
+                 "Episode II is nice",
+                 "Episode III is quite good",
+                 "Episode IV is super cool",
+                 "Episode V is wonderful",
+                 "Episode VI is super bad",
+                 "I felt asleep during Episode VII",
+                 "I watched Episode VIII two times",
+                 "I didn't want to see Episode IX",
+                 "Rogue One is a nice film",
+                 "I didn't see Solo"]
+
+num_rating_movies = [2, 3, 5, 10, 4, 1, 8, 9, 3, 2, 5]
+
 def list_declaration_1(a="Mozart", b=1.2):
     """
     Lists are mutable
@@ -33,8 +59,25 @@ def list_change_element(my_list: list, index, new_element="default"):
     return print(my_list)
 
 
+def combine_two_lists(list_1, list_2):
+    """
+    Use the built-in 'zip' function: it takes two lists and
+    combines the fist two elements into a tuple, than make the
+    same thing for all the elements of the lists
+    :param list_1:
+    :param list_2:
+    :return:
+    """
+    my_list = []
+    for i in zip(list_1, list_2):
+        my_list.append(i)
+    return my_list
+
+
 if __name__ == '__main__':
     main_artist = list_declaration_1("Aladino")
     list_append(main_artist)
     s_list = list_sum(main_artist)
     list_change_element(s_list, 4)
+    print("\n")
+    print(combine_two_lists(star_wars_movies, rating_movies))
