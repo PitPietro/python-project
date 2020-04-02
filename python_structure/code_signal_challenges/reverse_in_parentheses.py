ef9ec66eab8d4b7ebaeb2caca68664c074d7dcca
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-02 17:27:23 +0200

    reverse_in_parentheses 1st attemp

--- python_structure/code_signal_challenges/reverse_in_parentheses.py
+++ python_structure/code_signal_challenges/reverse_in_parentheses.py
@@ -1,3 +1,4 @@
+from python_structure.data_structures.stacks_queues.stack import Stack
 """
 > Task
 Write a function that reverses characters in (possibly nested) parentheses in the input string.
@@ -22,8 +23,37 @@ Return input_string, with all the characters that were in parentheses reversed.
 """
 
 
+def reverse(string):
+    stack = Stack()
+    for i in string:
+        stack.push(i)
+
+    reversed_s = ""
+    for i in range(stack.size()):
+        reversed_s += stack.pop()
+
+    return reversed_s
+
+
 def reverse_in_parentheses(input_string):
-    pass
+    normal_s = ""
+    rev_s = ""
+    for i, s in enumerate(input_string):
+        if s == "(":
+            rev_s = ""
+            rev_s += s
+        elif s == ")":
+            normal_s += reverse(rev_s)
+        else:
+            normal_s += s
+        print("i: {}| s: {}| string: {}|  rev_s: {}".format(i, s, normal_s, rev_s))
+    return normal_s
+
+
+def test_enumerate(string_to_e):
+    for i, s in enumerate(string_to_e):
+        i += 1
+        print("i: {}| s: {}| string: {}".format(i, s, string_to_e))
 
 
 if __name__ == '__main__':
@@ -32,7 +62,9 @@ if __name__ == '__main__':
     test_3 = "foo(bar)baz(blim)"
     test_4 = "foo(bar(baz))blim"
 
+    # test_enumerate("abcde")
+
     print(reverse_in_parentheses(test_1))
-    print(reverse_in_parentheses(test_2))
-    print(reverse_in_parentheses(test_3))
-    print(reverse_in_parentheses(test_4))
+    # print(reverse_in_parentheses(test_2))
+    # print(reverse_in_parentheses(test_3))
+    # print(reverse_in_parentheses(test_4))
