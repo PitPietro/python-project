12084816c82a797348a44106ca511b95f3cf4ddc
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-04 18:57:12 +0200

    add_border solved

--- python_structure/code_signal_challenges/add_border.py
+++ python_structure/code_signal_challenges/add_border.py
@@ -22,23 +22,31 @@ def add_border(picture):
     print("border len: {}".format(border))
     print("picture len: {}".format(len(picture)))
     res_list = []
-    for i in range(len(picture) + 2):
-        res_list.append([])
-        print("result list: {}".format(res_list))
 
-    for i, j in enumerate(res_list):
-        if i == 0 or i == len(res_list) - 1:
-            j.append("*" * border)
+    for i in range(len(picture) + 2):
+        if i == 0 or i == len(picture) + 1:
+            res_list.append("*" * border)
         else:
             tmp_string = "*"
-            # j += "*"
             tmp_string += picture[i - 1]
-            # j += "*"
             tmp_string += "*"
-            j.append(tmp_string)
+            res_list.append(tmp_string)
             print("tmp string: {}".format(tmp_string))
-        print("i: {}| j: {}".format(i, j))
+        print("result string: {}| i: {}".format(res_list, i))
     return res_list
+
+
+def add_border_v2(picture):
+    borders = len(picture[0]) + 2
+    return ["*" * borders] + [x.center(borders, "*") for x in picture] + ["*" * borders]
+
+
+def add_border_v3(picture):
+    result = ['*' * (len(picture[0]) + 2)]
+    for i in picture:
+        result.append('*' + i + '*')
+    result.append(result[0])
+    return result
 
 
 if __name__ == '__main__':
@@ -49,7 +57,7 @@ if __name__ == '__main__':
     test_5 = ["wzy**"]
 
     print(add_border(test_1))
-    print(add_border(test_2))
-    print(add_border(test_3))
-    print(add_border(test_4))
-    print(add_border(test_5))
+    # print(add_border(test_2))
+    # print(add_border(test_3))
+    # print(add_border(test_4))
+    # print(add_border(test_5))
