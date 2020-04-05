0585f726f4b1e4ebc463829145539fb8a294e770
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-05 17:05:57 +0200

    are similar test 8

--- python_structure/code_signal_challenges/are_similar.py
+++ python_structure/code_signal_challenges/are_similar.py
@@ -31,8 +31,28 @@ true if a and b are similar, false otherwise.
 def are_similar(a, b):
     if a == b:
         return True
+    if not diff(a, b):
+        for i in range(len(a)):
+            for j in range(len(b) - 1):
+                print("i: {}| j: {}| b[i]: {}| b[i + 1]: {}| b: {}".format(i, j, b[i], b[i + 1], b))
+                temp_b = b
+                temp_b[j], temp_b[j + 1] = temp_b[j + 1], temp_b[j]
+
+                if temp_b == a:
+                    return True
+        return False
     else:
         return False
+
+
+def diff(list_1, list_2):
+    """
+    get difference of two lists
+    :param list_1: list
+    :param list_2: list
+    :return: list
+    """
+    return list(set(list_1) - set(list_2))
 
 
 if __name__ == '__main__':
@@ -59,7 +79,7 @@ if __name__ == '__main__':
     test_7_b = [10, 3, 2]
     # false
     test_8_a = [4, 6, 3]
-    test_8_b = [4, 6, 3]
+    test_8_b = [3, 4, 6]
     # true
     test_9_a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
     test_9_b = [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
@@ -67,13 +87,13 @@ if __name__ == '__main__':
     test_10_a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
     test_10_b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]
 
-    print(are_similar(test_1_a, test_1_b))
-    print(are_similar(test_2_a, test_2_b))
-    print(are_similar(test_3_a, test_3_b))
-    print(are_similar(test_4_a, test_4_b))
-    print(are_similar(test_5_a, test_5_b))
-    print(are_similar(test_6_a, test_6_b))
-    print(are_similar(test_7_a, test_7_b))
+    # print(are_similar(test_1_a, test_1_b))
+    # print(are_similar(test_2_a, test_2_b))
+    # print(are_similar(test_3_a, test_3_b))
+    # print(are_similar(test_4_a, test_4_b))
+    # print(are_similar(test_5_a, test_5_b))
+    # print(are_similar(test_6_a, test_6_b))
+    # print(are_similar(test_7_a, test_7_b))
     print(are_similar(test_8_a, test_8_b))
-    print(are_similar(test_9_a, test_9_b))
-    print(are_similar(test_10_a, test_10_b))
+    # print(are_similar(test_9_a, test_9_b))
+    # print(are_similar(test_10_a, test_10_b))
