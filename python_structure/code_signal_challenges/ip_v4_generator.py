6af49b78561db92b269bac94612f4634881346fc
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-11 10:50:25 +0200

    ip_v4_generator _.j.k.y & time calc

--- python_structure/code_signal_challenges/ip_v4_generator.py
+++ python_structure/code_signal_challenges/ip_v4_generator.py
@@ -1,20 +1,27 @@
 """
 This is not a Code Signal task: it prints all the possible IPv4 addresses
 """
+import time
 
 
 def ip_v4_generator():
     range_n = 256
     iterator = 0
-    for k in range(range_n):
+    for j in range(range_n):
         iterator += 1
-        k_str = "." + str(k)
-        for y in range(range_n):
+        j_str = "." + str(j)
+        for k in range(range_n):
             iterator += 1
-            y_str = "." + str(y)
-            result = k_str + y_str
-            print("{}\tn° {}".format(result, iterator))
+            k_str = "." + str(k)
+            for y in range(range_n):
+                iterator += 1
+                y_str = "." + str(y)
+                result = j_str + k_str + y_str
+                print("{}\tn° {}".format(result, iterator))
 
 
 if __name__ == '__main__':
+    start_i = time.clock()
     ip_v4_generator()
+    end_i = time.clock()
+    print("time: {} seconds".format(end_i - start_i))
