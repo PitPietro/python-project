9119721c30902114f073b596f1558db6dcdc58f7
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-11 12:10:50 +0200

    ip_v4_generator i.j.k.y & time calc

--- python_structure/code_signal_challenges/ip_v4_generator.py
+++ python_structure/code_signal_challenges/ip_v4_generator.py
@@ -7,17 +7,20 @@ import time
 def ip_v4_generator():
     range_n = 256
     iterator = 0
-    for j in range(range_n):
+    for i in range(range_n):
         iterator += 1
-        j_str = "." + str(j)
-        for k in range(range_n):
+        i_str = str(i)
+        for j in range(range_n):
             iterator += 1
-            k_str = "." + str(k)
-            for y in range(range_n):
+            j_str = "." + str(j)
+            for k in range(range_n):
                 iterator += 1
-                y_str = "." + str(y)
-                result = j_str + k_str + y_str
-                print("{}\tn° {}".format(result, iterator))
+                k_str = "." + str(k)
+                for y in range(range_n):
+                    iterator += 1
+                    y_str = "." + str(y)
+                    result = i_str + j_str + k_str + y_str
+                    print("{}\tn° {}".format(result, iterator))
 
 
 if __name__ == '__main__':
