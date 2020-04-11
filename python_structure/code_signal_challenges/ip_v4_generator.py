2f4cbe0fca52412ec8a47b8eb3f613ee1eaf4dea
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-04-11 10:38:28 +0200

    ip_v4_generator _._._.y

--- python_structure/code_signal_challenges/ip_v4_generator.py
+++ python_structure/code_signal_challenges/ip_v4_generator.py
@@ -4,7 +4,9 @@ This is not a Code Signal task: it prints all the possible IPv4 addresses
 
 
 def ip_v4_generator():
-    pass
+    for y in range(256):
+        y_str = "." + str(y)
+        print(y_str)
 
 
 if __name__ == '__main__':
