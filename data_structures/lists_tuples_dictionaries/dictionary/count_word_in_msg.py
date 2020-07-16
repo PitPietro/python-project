2e6d2c6011573efd959091739ba2d799adb3db1c
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-16 12:55:45 +0200

    Count message words - TODOs

--- data_structures/lists_tuples_dictionaries/dictionary/count_word_in_msg.py
+++ data_structures/lists_tuples_dictionaries/dictionary/count_word_in_msg.py
@@ -1,16 +1,22 @@
+import pprint
+
 message = ''
 count = {}
 
 
 def count_letters():
-    for character in message:
+    for character in message.lower():
         count.setdefault(character, 0)
         count[character] = count[character] + 1
 
-    print(count)
+    final_dict = pprint.pformat(count)
+    print(final_dict)
 
 
 if __name__ == '__main__':
     message = input('Insert a world or a phrase: ')
     count_letters()
     # TODO: save the result in a SVG file (whit the commas)
+    # TODO: replace the input whit the reading of a .txt file
+    #  (it allows the study of the frequency for a specific cryptography method)
+    # TODO: Make a cake diagram from it and display it whit pandas!
