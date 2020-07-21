8ca93c28706162296130b91475733568fccee285
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-21 15:45:24 +0200

    find all regex - phne number

--- regular_expression/re_find_number.py
+++ regular_expression/re_find_number.py
@@ -3,5 +3,5 @@ import re
 if __name__ == '__main__':
     message = 'Call 123-332-2211 from 10 am to 10pm or 667-889-4453 during the night'
     usa_phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-    result = usa_phone_num_regex = re.findall(message)
+    result = usa_phone_num_regex.findall(message)
     print(result)
