7223aa0b0720094fca72be8664b1fd09034eea51
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-06-01 23:39:30 +0200

    day slice and javadocs

--- artificial_intelligence/jarvis/hello_world/tell_current_time.py
+++ artificial_intelligence/jarvis/hello_world/tell_current_time.py
@@ -1,17 +1,41 @@
 # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+# https://stackoverflow.com/questions/3898572/what-is-the-standard-python-docstring-format
 import pyttsx3
 import datetime
 
 
 def speak(audio):
+    """
+    :param audio: string to be spoken
+    """
     engine.say(audio)
     print(audio)
     engine.runAndWait()
 
 
-def get_time():
+def get_time_and_time():
+    """
+    Get the current date and time and speak it
+    """
     current_time = datetime.datetime.now().strftime("%d %B %Y %H:%M:%S")
+    speak("Current date is ")
     speak(current_time)
+
+
+def day_slice():
+    """
+    Get the current day slice based on the hour and greet the user by consequence
+    """
+    hour = datetime.datetime.now().hour
+
+    if 0 <= hour < 6:
+        speak("Good sunrise")
+    if 6 <= hour < 12:
+        speak("Good morning")
+    if 12 <= hour < 18:
+        speak("Good afternoon")
+    if 18 <= hour < 24:
+        speak("Good night")
 
 
 if __name__ == '__main__':
@@ -19,4 +43,5 @@ if __name__ == '__main__':
 
     # slow down voice rate
     engine.setProperty('rate', 150)
-    get_time()
+    get_time_and_time()
+    day_slice()
