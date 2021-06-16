a603457cdf13d9eb5aeb44f8ae98e7778c3b7cfa
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-06-16 14:46:13 +0200

    basics numpy array functions

--- scientific-computing/numpy-basics/numpy-array.py
+++ scientific-computing/numpy-basics/numpy-array.py
@@ -5,11 +5,17 @@
 #
 # Elements inside an array are all of the same type, referred to as the array 'dtype'.
 #
+# 'ndarray' is a shorthand for 'N-dimensional array'.
+# An N-dimensional array is an array with any number of dimensions.
+# NumPy 'ndarray' class is used to represent both matrices and vectors.
+# A vector is an array with a single dimension (no difference between row and column vectors).
+# A matrix refers to an array with two dimensions.
+# For 3-D or higher dimensional arrays, 'tensor' is also commonly used as term.
+#
 # An array can be indexed by a tuple of non-negative integers, by booleans, by another array, or by integers.
 # The rank of the array is the number of dimensions.
 # The shape of the array is a tuple of integers giving the size of the array along each dimension.
 #
-# One way we can initialize NumPy arrays is from Python lists, using nested lists for two- or higher-dimensional data.
 #
 # An array can be indexed by:
 # - a tuple of non-negative integers
@@ -27,13 +33,25 @@ import numpy as np
 
 def init_np_array(array):
     """
-    Initialize a NumPy array from a Python array
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
+    Create a NumPy array from a Python array
     """
-    print(np.array(array))
+    return np.array(array)
 
 
 def number_of_axes(array):
     """
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
     Number of axes (dimensions) of the array.
     """
     return np.array(array).ndim
@@ -41,26 +59,84 @@ def number_of_axes(array):
 
 def shape(array):
     """
-    Dimensions of the array. It is a tuple of integers indicating the size of the array in each dimension.
+    The dimension of the array is a tuple of integers indicating the size of the array in each dimension.
     For a matrix with n rows and m columns, shape will be (n,m).
     The length of the shape tuple is therefore the number of axes, ndim.
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
+    Dimensions of the array.
     """
     return np.array(array).shape
 
 
 def size(array):
     """
+    Parameters
+    ----------
+    array Python array Python array
+
+    Returns
+    -------
     Total number of elements of the array. It is equal to the product of the elements of shape.
     """
     return np.array(array).size
 
 
+def dtype(array):
+    """
+    One can create or specify dtype's using standard Python types.
+    NumPy provides types of its own. numpy.int32, numpy.int16, and numpy.float64 are some examples.
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
+    Object describing the type of the elements in the array.
+    """
+    return np.array(array).dtype
+
+
+def item_size(array):
+    """
+
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
+    Size in bytes of each element of the array.
+    """
+    return np.array(array).itemsize
+
+
+def data(array):
+    """
+    Parameters
+    ----------
+    array Python array
+
+    Returns
+    -------
+    The buffer containing the actual elements of the array.
+    You do not usually need to use this attribute: you will access the elements in an array using indexing facilities.
+    """
+    return np.array(array).data
+
+
 if __name__ == '__main__':
     py_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     py_arrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
-    init_np_array(py_array)
-    print('')
-    init_np_array(py_arrays)
+    print('mono-dimensional array:\n' + str(init_np_array(py_array)))
+    print('\nn-dimensional array:\n' + str(init_np_array(py_arrays)))
     print('number of axes: ' + str(number_of_axes(py_arrays)))
     print('shape: ' + str(shape(py_arrays)))
     print('n° of elements: ' + str(size(py_arrays)))
+    print('dtype: ' + str(dtype(py_arrays)))
+    print('item size: ' + str(item_size(py_arrays)))
+    print('data: ' + str(data(py_arrays)))
