40d9623fcf35cfc76cff985717d48193227ac62c
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-06-22 14:44:00 +0200

    numpy - ufunc - plotting test

--- math/numpy_basics/universal_functions/trigonometric_functions.py
+++ math/numpy_basics/universal_functions/trigonometric_functions.py
@@ -3,7 +3,7 @@
 # Within NumPy, these functions operate elementwise on an array, producing an array as output.
 # All trigonometric functions use radians when an angle is called for.
 import math
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -64,6 +64,23 @@ if __name__ == '__main__':
     print('tan:\n' + str(tan_array))
 
     # plotting
+    f_x = np.linspace(-2, 2, 100)
+    f_y = np.zeros((1, 100))
+
+    for i in range(f_x.size):
+        f_y[0][i] = np.sin(f_x[i])
+
+    plt.figure(1)
+    plt.plot(f_x, f_y[0])
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
+
+    # Add a horizontal line across the axis.
+    plt.axhline(y=0, color='k')
+
+    # Add a vertical line across the axes.
+    plt.axvline(x=0, color='k')
+    plt.show()
 
 # https://numpy.org/doc/stable/reference/ufuncs.html
 # https://numpy.org/doc/stable/reference/ufuncs.html#trigonometric-functions
