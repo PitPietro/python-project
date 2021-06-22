f0598aca6e2c52425162e9b72fd19ff1bf159b8a
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-06-22 11:00:41 +0200

    numpy - ufunc - trig func file

--- math/numpy_basics/universal_functions/trigonometric_functions.py
+++ math/numpy_basics/universal_functions/trigonometric_functions.py
@@ -6,8 +6,14 @@ import numpy as np
 
 
 def _fill_trigonometry_array():
+    """
+    Utility function to fill the trigonometry array
+    Returns
+    -------
+    The array filled with useful angle measures
+    """
     trig_array = [
-        0,  # 0
+        0,  # 0°
         math.pi / 6,  # 30°
         math.pi / 4,  # 45°
         math.pi / 3,  # 60°
@@ -16,6 +22,13 @@ def _fill_trigonometry_array():
         (3 * math.pi) / 4,  # 135°
         (5 * math.pi) / 6,  # 150°
         math.pi,  # 180°
+        (7 * math.pi) / 6,  # 210°
+        (5 * math.pi) / 4,  # 225°
+        (4 * math.pi) / 3,  # 240°
+        (3 * math.pi) / 2,  # 270°
+        (5 * math.pi) / 3,  # 300°
+        (7 * math.pi) / 4,  # 315°
+        (11 * math.pi) / 6,  # 330°
     ]
     return trig_array
 
@@ -34,7 +47,10 @@ if __name__ == '__main__':
     sin_array = np_sin(trigonometry_array)
     cos_array = np_cos(trigonometry_array)
 
-    print('Numpy Array - Universal Functions\n')
+    print('Numpy Array - Universal Functions - Trigonometric\n')
     print('-------\nTrigonometry:\n')
     print('sin:\n' + str(sin_array))
     print('cos:\n' + str(cos_array))
+
+# https://numpy.org/doc/stable/reference/ufuncs.html
+# https://numpy.org/doc/stable/reference/ufuncs.html#trigonometric-functions
