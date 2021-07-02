041e83e9da8d4951dcdb1ae9a4dc8af94824c5e5
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-07-02 15:28:53 +0200

    trying to get LaTeX work with matplotlib in mac os

--- math_study/analysis/function_plotting/graph_transformations/translation_on_y_axis.py
+++ math_study/analysis/function_plotting/graph_transformations/translation_on_y_axis.py
@@ -1,19 +1,30 @@
 import math
-import numpy as np
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import numpy as np
 
 if __name__ == '__main__':
+    # init config
     x_limit = math.pi * 2
+    rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+    # for 'Palatino' and other serif fonts use:
+    # rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
 
+    # domain and co-domain settings
     x = np.linspace(-x_limit, x_limit, 100)
     f_x = np.sin(x)
 
     c = 2
+
+    # upper function
     g_x = f_x + c
+
+    # lower function
     h_x = f_x - c
 
-    plt.figure(1)  # plt.figure(1, figsize=(10, 5))  # nop
+    plt.figure(1)
 
     plt.plot(x, g_x, 'y--')
     plt.plot(x, f_x)
@@ -31,8 +42,15 @@ if __name__ == '__main__':
     # add a vertical line across the axes
     plt.axvline(x=0, color='k')
 
+    # *math_fontfamily* can be used in most places where there is text,
+    # like in the title:
+    plt.title(r"$f(x)\pm c$", size=14)  # , **code_font
+
     # display all open figures
     plt.show()
 
+    # print(matplotlib.font_manager.fontManager.ttflist)
+
+# https://stackoverflow.com/questions/21321670/how-to-change-fonts-in-matplotlib-python#21323217
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
 # https://python.astrotech.io/matplotlib/multiple-figures.html#multiple-plots-on-one-figure
