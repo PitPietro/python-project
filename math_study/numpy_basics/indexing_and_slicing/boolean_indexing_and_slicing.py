a2dfd9e655cdd4eb35cb4dd8236ca7957ed94c3c
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-07-07 11:35:53 +0200

    numpy - slicing and indexing - advanced

--- math_study/numpy_basics/indexing_and_slicing/boolean_indexing_and_slicing.py
+++ math_study/numpy_basics/indexing_and_slicing/boolean_indexing_and_slicing.py
@@ -21,13 +21,36 @@ if __name__ == '__main__':
     print('\nordered list of integers')
     print(data)
 
-    print("\ncheck where is an element in an array and return another array filled with 'True' or 'False'")
+    print("\ncheck where is the given element in the array and return an array filled with 'True' or 'False'")
     print(words == 'He')
+
+    print("\ncheck where there is not the given element in the array and return an array filled with 'True' or 'False'")
+    print(words != 'He')
+
+    print('\n(negate the condition in a different way)')
+    print(~(words == 'He'))
 
     # 'data' is a multidimensional array filled with ordered number (to make easier to understand)
     # returns the sub-arrays corresponding to the indexes equal to 'True' depending on the statement in the []
     print("\ntake the arrays corresponding to the indexes which the condition set to 'True'")
     print(data[words == 'Hu'])
+
+    print('\n(negate the condition)')
+    print(data[~(words == 'Hu')])
+
+    print('\nsave the condition in a variable')
+    condition = (words == 'Ha')
+    print(condition)
+    print('\ntake the arrays corresponding to the indexes by negating the saved condition')
+    print(data[~condition])
+
+    print('\nsave the multiple condition in a variable')
+    multiple_condition = (words == 'Ha') | (words == 'He')
+    print(multiple_condition)
+    print("\ntake the arrays corresponding to the indexes which the multiple condition set to 'True'")
+    print(data[multiple_condition])
+    print("\n(negate the multiple condition)")
+    print(data[~multiple_condition])
 
     print("\nmoreover, slice the output result (in different ways)")
     print(data[words == 'Hu', :])
@@ -40,3 +63,14 @@ if __name__ == '__main__':
 
     print('\ncreate an array from the condition and the given index position')
     print(data[words == 'Hu', 3])
+
+    print('\nfilter the data where the array elements correspond to the given condition and assign them')
+    tmp_data = np.copy(data)  # use a copy of the array to avoid reassign the whole data (demonstration purpose only)
+    tmp_data[tmp_data < 40] = 0
+    print(tmp_data)
+    print('\n(conditions can be different)')
+    tmp_data = np.copy(data)  # copy again the data array to let the test starting from the "standard" data
+    tmp_data[words == 'Hi'] = 0
+    print(tmp_data)
+
+# https://numpy.org/doc/stable/reference/generated/numpy.copy.html
